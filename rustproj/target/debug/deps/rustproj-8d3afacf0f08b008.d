008fe749f60f96e32d742dec42d7135e8d11ff6a
bliss06 <85256374+bliss06@users.noreply.github.com>
2023-04-16 22:21:29 +0530

    delete rust/target dir

--- rustproj/target/debug/deps/rustproj-8d3afacf0f08b008.d
+++
@@ -1,5 +0,0 @@
-/home/deepak/dev/rustproj/target/debug/deps/rustproj-8d3afacf0f08b008: src/main.rs
-
-/home/deepak/dev/rustproj/target/debug/deps/rustproj-8d3afacf0f08b008.d: src/main.rs
-
-src/main.rs:
--- rustproj/target/debug/rustproj.d
+++
@@ -1,1 +0,0 @@
-/home/deepak/dev/rustproj/target/debug/rustproj: /home/deepak/dev/rustproj/src/main.rs
